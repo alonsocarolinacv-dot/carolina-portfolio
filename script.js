05b3399a3085d34118b6f9a4e3a961cc9c3f1be2
alonsocarolinacv-dot <alonso.carolina.cv@gmail.com>
2026-01-04 12:13:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("year").textContent = new Date().getFullYear();
 
+// Limpia parámetros feos del URL (Formspree redirect)
+if (location.search) {
+  history.replaceState({}, document.title, location.pathname + location.hash);
+}
